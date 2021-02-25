6036df01c960fff63d5ec933a8aa9a67fd8a9af3
jyrilundin <jyri.lundin@gmail.com>
2021-02-25 12:57:19 +0200

    updated index.js

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const express = require('express')
 express.static('public')
 const app = express()
 app.use(express.static('public'))
-const port = 8080
+const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
   res.send('Welcome to my server!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port, () => { 8080 })
