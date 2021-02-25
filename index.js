8d064377ee56a49fab6d4732a0cc976dc9335908
jyrilundin <jyri.lundin@gmail.com>
2021-02-25 10:37:29 +0200

    Hellow world! testing

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const server = http.createServer((request, response) => {
     response.end("Path; " + request.url);
 });
 
+var express = require('express')
+var app = express()
+
+
+express.static(root, [options])
+app.use(express.static('public'))
+
 server.listen(8080, () => {
     console.log(`Server istening in port ${server.address().port}`);   
 });
